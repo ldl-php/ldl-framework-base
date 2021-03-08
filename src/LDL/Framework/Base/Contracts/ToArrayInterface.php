076adcf13ff5091b00c678c5499909c5ebb4d060
Federico <jpfstange@gmail.com>
2021-03-08 08:45:23 -0300

    Add ToArray and ArrayFactory exceptions

--- src/LDL/Framework/Base/Contracts/ToArrayInterface.php
+++ src/LDL/Framework/Base/Contracts/ToArrayInterface.php
@@ -2,7 +2,13 @@
 
 namespace LDL\Framework\Base\Contracts;
 
+use LDL\Framework\Base\Exception\ToArrayException;
+
 interface ToArrayInterface
 {
+    /**
+     * @throws ToArrayException
+     * @return array
+     */
     public function toArray() : array;
 }
