377cf3deb9c40bdf348d396c4482d601914b340d
Federico <jpfstange@gmail.com>
2021-04-14 09:31:24 -0300

    feature/1199522418295938 - Add iterable helper toArray, map and filter

--- src/LDL/Framework/Helper/IterableHelper.php
+++ src/LDL/Framework/Helper/IterableHelper.php
@@ -16,4 +16,46 @@ abstract class IterableHelper
 
         return count(\iterator_to_array($items));
     }
+
+    /**
+     * @param iterable $items
+     *
+     * @return array
+     */
+    public static function toArray(iterable $items) : array
+    {
+       if(is_array($items)){
+          return $items;
+       }
+
+       if($items instanceof \Traversable){
+          return \iterator_to_array($items, true);
+       }
+
+       return [];
+    }
+
+    /**
+     * @param iterable $items
+     * @param callable $func
+     *
+     * @return array
+     */
+    public static function map(iterable $items, callable $func) : array
+    {
+       return array_map($func, self::toArray($items));    
+    }
+
+    /**
+     * @param iterable $items
+     * @param callable $func
+     * @param int $mode
+     *
+     * @return array
+     */
+    public static function filter(iterable $items, callable $func, int $mode=0) : array
+    {
+       return array_filter(self::toArray($items), $func, $mode);
+    }
+
 }
